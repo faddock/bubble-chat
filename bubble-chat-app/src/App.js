a5fa8a88936ad4dcc1384a967c45f0d63aed272e
faddock <61219240+faddock@users.noreply.github.com>
2020-11-26 00:40:13 +0530

    button and default message

---
+++ bubble-chat-app/src/App.js
@@ -0,0 +1,59 @@
+//import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+
+
+
+function App() {
+  //input constant 
+  const[input, setInput] = useState('');
+  //stores messages in an array which has default messages.
+  const [messages, setMessages] = useState(['Welcome to Bubbles','Suyash']);
+  //send Message event
+  const sendMessage = (event) => {
+    event.preventDefault();     //stops form submit refreshing page
+    setMessages([...messages, input]);      //...messages allows previous messages to remain.
+    setInput('');     //clears the input for new.
+  }
+  console.log(messages);
+  
+  
+  
+  return (
+    
+    <div className="App">
+      <h1>Hello Bubbles users!</h1>
+    <form> 
+      <input value={input} onChange={event => setInput(event.target.value)}></input>
+      <Button variant='contained' type="submit" onClick={sendMessage}>my button</Button>
+    </form>   
+     {/*messages themsleves*/}
+     {messages.map(message => (
+       <p>{message}</p>
+     ))} 
+
+    </div>
+    
+  );
+  
+}
+
+export default App;
+
+/*
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+*/  
